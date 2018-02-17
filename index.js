94ddad055a7d8079f6f62a1f0233af8db051c556
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-02-17 18:21:15 +0100

    Feat: turns the led on when is dark

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const eight = require("johnny-five");
-const board = new eight.Board();
+var five = require("johnny-five"),
+  board,
+  photoresistor;
 
-board.on("ready", () => {
-  const led = new eight.Led(8);
-  led.blink(500);
+board = new five.Board();
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led
+  });
+
+  // Create a new `photoresistor` hardware instance.
+  photoresistor = new five.Sensor({
+    pin: "A2",
+    freq: 250
+  });
+
+  // Inject the `sensor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    pot: photoresistor
+  });
+
+  // "data" get the current reading from the photoresistor
+  photoresistor.on("data", function() {
+    console.log(this.value);
+    if (this.value > 1000) led.blink();
+    else led.stop();
+  });
 });
